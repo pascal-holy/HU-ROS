bb460320e98e805898863cee12adcb68b3bd7e90
Pascal Holy <holy.pascal@gmail.com>
2016-06-12 14:01:16 +0200

    bidding framework

--- src/beginner_tutorials/scripts/classes/job.py
+++ src/beginner_tutorials/scripts/classes/job.py
@@ -4,16 +4,20 @@
 
 class Job(object):
 
-    def __init__(self, id, source, destination):
+    def __init__(self, id, cost, source, destination):
         self.id = id
+        self.cost = cost
         self.source = source
         self.destination = destination
+        self.payload = ""
 
     def printJob(self):
         print "id: %s" % self.id
+        print "cost: %s" % self.cost
         print "source: (%s, %s, %s)" % (self.source[0],
                                         self.source[1],
                                         self.source[2])
         print "destination: (%s, %s, %s)" % (self.destination[0],
                                              self.destination[1],
                                              self.destination[2])
+        print "payload: %s" % self.payload
