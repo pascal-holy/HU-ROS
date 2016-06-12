7dd4a3947a9b394c1ed5b72401ab925cadb72f13
Pascal Holy <holy.pascal@gmail.com>
2016-06-12 18:39:57 +0200

    deleted beginner tutorials

--- src/beginner_tutorials/CMakeFiles/_beginner_tutorials_generate_messages_check_deps_Dst.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_beginner_tutorials_generate_messages_check_deps_Dst"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_beginner_tutorials_generate_messages_check_deps_Dst.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
